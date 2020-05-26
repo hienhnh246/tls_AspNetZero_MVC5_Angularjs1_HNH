f5eac49906f37acbafb9937bb71f470d94cad5b0
Huynh Hien <hien.hnh246@gmail.com>
2020-05-26 23:48:40 +0700

    Initialize the Project

---
+++ src/Tests/triluatsoft.tls.Tests/tlsTestModule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Abp.Modules;
+using Abp.MultiTenancy;
+using Abp.TestBase;
+using Abp.Zero.Configuration;
+using Castle.MicroKernel.Registration;
+using NSubstitute;
+
+namespace triluatsoft.tls.Tests
+{
+    [DependsOn(
+        typeof(tlsApplicationModule),
+        typeof(tlsDataModule),
+        typeof(AbpTestBaseModule))]
+    public class tlsTestModule : AbpModule
+    {
+        public override void PreInitialize()
+        {
+            Configuration.UnitOfWork.Timeout = TimeSpan.FromMinutes(30);
+
+            //Use database for language management
+            Configuration.Modules.Zero().LanguageManagement.EnableDbLocalization();
+
+            RegisterFakeService<IAbpZeroDbMigrator>();
+        }
+
+        private void RegisterFakeService<TService>() 
+            where TService : class
+        {
+            var instance = Substitute.For<TService>();
+            IocManager.IocContainer.Register(
+                Component.For<TService>()
+                    .Instance(instance)
+                    .LifestyleSingleton()
+            );
+        }
+    }
+}
